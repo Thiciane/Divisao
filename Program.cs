2c8ac266aee00718578e4c24eb2fe635f9fdd32b
Thici <thicianefernanda892@gmail.com>
2023-05-30 15:03:12 -0300

    Divisão simples em C#
    Divisão simples para teste do GitHub

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Informe dois números: ");
+
+double numero1 = Convert.ToDouble(Console.ReadLine());
+double numero2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(numero1/numero2);
+Console.ReadKey();
